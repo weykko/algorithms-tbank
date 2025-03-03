8f19ddcc950d90156efc54f215fcd415c2fb87a1
aleksandr <a.d.beznutrov@urfu.me>
2025-03-03 22:41:07 +0500

    heap task new solution

--- contest 3/D.py
+++ contest 3/D.py
@@ -1,29 +1,51 @@
-# class Heap:
-#     def __init__(self):
-#         self.heap = []
+class MyHeap:
+    def __init__(self):
+        self.heap = []
 
+    def _lower(self, i):
+        largest = i
+        left = 2 * i + 1
+        right = 2 * i + 2
+
+        if left < len(self.heap) and self.heap[left] > self.heap[largest]:
+            largest = left
+
+        if right < len(self.heap) and self.heap[right] > self.heap[largest]:
+            largest = right
+
+        if largest != i:
+            self.heap[i], self.heap[largest] = self.heap[largest], self.heap[i]
+            self._lower(largest)
+
+    def _raise(self, i):
+        parent = (i - 1) // 2
+
+        if i > 0 and self.heap[i] > self.heap[parent]:
+            self.heap[i], self.heap[parent] = self.heap[parent], self.heap[i]
+            self._raise(parent)
+
+    def insert(self, item):
+        self.heap.append(item)
+        self._raise(len(self.heap) - 1)
+
+    def extract(self):
+        if len(self.heap) == 0:
+            return None
+
+        item = self.heap[0]
+        self.heap[0] = self.heap[-1]
+        self.heap.pop()
+        self._lower(0)
+
+        return item
 
 
 n = int(input())
-heap = []
+heap = MyHeap()
 
 for _ in range(n):
     t = input().split()
     if t[0] == '0':
-        heap.append(int(t[1]))
-        i = len(heap) - 1
-        while i > 0 and heap[(i - 1) // 2] < heap[i]:
-            heap[i], heap[(i - 1) // 2] = heap[(i - 1) // 2], heap[i]
-            i = (i - 1) // 2
+        heap.insert(int(t[1]))
     else:
-        print(heap[0])
-        heap[0] = heap[-1]
-        heap.pop()
-        i = 0
-        while (2 * i + 1 < len(heap) and heap[2 * i + 1] > heap[i]) or (2 * i + 2 < len(heap) and heap[2 * i + 2] > heap[i]):
-            if 2 * i + 2 < len(heap) and heap[2 * i + 2] >= heap[2 * i + 1]:
-                heap[i], heap[2 * i + 2] = heap[2 * i + 2], heap[i]
-                i = 2 * i + 2
-            else:
-                heap[i], heap[2 * i + 1] = heap[2 * i + 1], heap[i]
-                i = 2 * i + 1
+        print(heap.extract())
