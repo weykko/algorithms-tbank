e6705f0d24037096d5e0fd942779b94baa7954b8
aleksandr <a.d.beznutrov@urfu.me>
2025-05-07 17:33:23 +0500

    contest 10 B

--- contest 10/B.py
+++ contest 10/B.py
@@ -2,9 +2,6 @@ class MyDisjointSet:
     def __init__(self, n):
         self.rank = [1] * n
         self.parent = [i for i in range(n)]
-        self.size = [1] * n
-        self.min = [i for i in range(n)]
-        self.max = [i for i in range(n)]
 
     def find(self, x):
         if self.parent[x] != x:
@@ -16,29 +13,29 @@ class MyDisjointSet:
         y_set = self.find(y)
 
         if x_set == y_set:
-            return
+            return False
         if self.rank[x_set] < self.rank[y_set]:
             x_set, y_set = y_set, x_set
         self.parent[y_set] = x_set
         if self.rank[x_set] == self.rank[y_set]:
             self.rank[x_set] += 1
 
-        new_set = self.find(x)
-        self.min[new_set] = min(self.min[x_set], self.min[y_set])
-        self.max[new_set] = max(self.max[x_set], self.max[y_set])
-        self.size[new_set] = (self.size[x_set] + self.size[y_set])
-
-    def get(self, x):
-        x_set = self.find(x)
-        return self.min[x_set], self.max[x_set], self.size[x_set]
+        return True
 
 
 n, m = map(int, input().split())
 dset = MyDisjointSet(n + 1)
-
+edges = []
 for _ in range(m):
-    q = input().split()
-    if q[0] == 'union':
-        dset.union(int(q[1]), int(q[2]))
-    else:
-        print(' '.join(map(str, dset.get(int(q[1])))))
+    u, v, w = map(int, input().split())
+    edges.append((u, v, w))
+
+result = 0
+for u, v, w in sorted(edges, key=lambda x: x[2]):
+    if dset.union(u, v):
+        result += w
+        n -= 1
+        if n == 1:
+            break
+
+print(result)
